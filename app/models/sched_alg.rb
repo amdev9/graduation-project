d58a57d93c9a2c82051a70b832a0364982ed4f8c
Alexander Matveev <blackkorol@gmail.com>
2014-10-06 00:31:56 +0400

    algo..

--- app/models/sched_alg.rb
+++ app/models/sched_alg.rb
@@ -3,12 +3,96 @@
 DAY_HOURS=12 #Number of working hours per day
 DAYS_NUM=5     #Number of days in week
 
+
+
+class CChildView
+
+	include AlgorithmState
+	def initialize
+		_schedule = nil
+		_running  = false
+		Algorithm.getInstance.getObserver.setWindow(self)
+	end
+	
+	def setSchedule(  schedule)
+		if _schedule != nil
+	  		_schedule = nil
+		end
+		_schedule = schedule.makeCopy( false )
+	end
+	
+	def setNewState(state)
+	  _running = state == AS_RUNNING
+	end
+	def printer
+  		if  _schedule!=nil 
+			puts "Fitness: #{_schedule.getFitness} , Generation: #{Algorithm.getInstance.getCurrentGeneration}"	
+			classes = _schedule.getClasses
+			ci = 0
+			classes.each { |(c,p)|
+				t = p % ( nr * DAY_HOURS )
+				d = p / ( nr * DAY_HOURS ) + 1
+				r = t / DAY_HOURS
+				t % DAY_HOURS + 1
+				l = r % 2;
+				m = r / 2;
+				puts "#{c.getCourse.getName}\n#{c.getProfessor.getName}\n" 
+				ci +=5
+			}
+  		end
+	end
+end
+
+module AlgorithmState
+	AS_USER_STOPED = 1
+	AS_CRITERIA_STOPPED = 2
+	AS_RUNNING = 3
+end
+
+
+class ScheduleObserver
+
+	include AlgorithmState
+	def initialize
+		_window = nil 
+	end
+
+	def setWindow( window) 
+		 _window = window
+	end
+
+
+	#Handles event that is raised when algorithm finds new best chromosome
+	def newBestChromosome( newChromosome)
+		if _window != nil
+			_window.setSchedule( newChromosome )
+		end
+	end
+
+		#Handles event that is raised when state of execution of algorithm is changed
+	def evolutionStateChanged( newState)
+
+		if _window != nil
+			_window.setNewState( newState )
+		end
+		newState != AS_RUNNING ? releaseEvent : blockEvent  ##????
+	end
+end 
+ 
+
+
+
+
+
 class Schedule
+
+	include AlgorithmState
 		@mutationSize 
 		@numberOfCrossoverPoints 
 	    @crossoverProbability 
 		@mutationProbability  
 		@fitness
+	
 	def initialize ( numberOfCrossoverPoints,  mutationSize,
 				    crossoverProbability,  mutationProbability)
 		@mutationSize = mutationSize
@@ -259,9 +343,12 @@ end
 end
 
 
-Algorithm.instance = null
+Algorithm.instance = nil
 
 class Algorithm
+	
+	include AlgorithmState
+	
 	@chromosomes
 	@bestFlags
 	@bestChromosomes
@@ -296,7 +383,7 @@ class Algorithm
 			replaceByGeneration = numberOfChromosomes - trackBest
 			end
 	for i in 0..chromosomes.size - 1 
-		chromosomes[ i ] = null
+		chromosomes[ i ] = nil
 		bestFlags[ i ] = false
 	end
 		
@@ -305,16 +392,41 @@ class Algorithm
 	def getCurrentGeneration
 		@currentGeneration
 	end
-   def getInstance
-	if instance == null 
-		prototype = Schedule.new( 2, 2, 80, 3)
-		instance = Algorithm.new( 100, 8, 5, prototype) ####, new ScheduleObserver() ); ##
-	 end
-	return instance
-   end
+   
 	def freeInstance
-		@instance = null
+		@instance = nil
 	end
+
+
+
+def getInstance
+
+	#Thread.new
+	if _instance == nil 
+		// make prototype of chromosomes
+	    prototype =   Schedule.new( 2, 2, 80, 3 )
+		// make new global instance of algorithm using chromosome prototype
+		_instance =  Algorithm.new( 100, 8, 5, prototype, new ScheduleObserver() )
+	end
+	return _instance;
+end
+
+def getObserver
+	return _observer
+end
+
+def stop
+
+	#thread
+
+		if _state == AS_RUNNING 
+			_state = AS_USER_STOPED
+		end
+	#lock.Unlock()
+end
+
+
+
 	def start
 		if !prototype
 			return
@@ -371,10 +483,7 @@ class Algorithm
 	    end
 	
 
-	def stop
-		if  state == AS_RUNNING  
-			state = AS_USER_STOPED
-	end
+	
 	
 	def getBestChromosome
 		chromosomes[ bestChromosomes[ 0 ] ]

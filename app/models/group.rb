7e59929cd7649b0a90d382d8880ec49b4b40c28d
Alexander Matveev <blackkorol@gmail.com>
2014-10-19 14:23:32 +0000

    group.rb edited online with Bitbucket

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
 	has_many :cla_groups
-	has_many :cla, :through => :cla_groups
+	has_many :cla, :through => :cla_groups, :dependent => :destroy
 	
 	def tname 
 		"#{name}"

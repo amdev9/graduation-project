3f74e51d7b38c51d4b84a18f02a5673bd93d9241
Alexander Matveev <blackkorol@gmail.com>
2014-10-19 14:18:27 +0000

    cla.rb edited online with Bitbucket_depend_destr

--- app/models/cla.rb
+++ app/models/cla.rb
@@ -2,7 +2,7 @@ class Cla < ActiveRecord::Base
 	has_one :professor    	
 	has_one :course
 	has_many :cla_groups
-	has_many :groups , :through => :cla_groups 
+	has_many :groups , :through => :cla_groups, :dependency => :destroy
 
 	def professor_name
 		professor.tname if professor
